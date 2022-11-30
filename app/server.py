ff8a60153f0b98af1c85a3f2c4c475896ec0609b
mwasib <91374619+mwasib@users.noreply.github.com>
2022-11-30 20:30:52 +0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/ pulmonary disease prediction model.pth'
+export_file_name = 'pulmonary disease prediction model.pth'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Covid-19', 'No_findings', 'Pneumonia']
 path = Path(__file__).parent
 
 app = Starlette()
